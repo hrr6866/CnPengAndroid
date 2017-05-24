7bbf4002c8986833341f7aa3f3f10a4596337bb6
CnPeng <893612134@qq.com>
2017-05-24 22:24:39 +0800

    a——39 增加只显示年和月的DatePickerDialog

--- app/src/main/java/com/cnpeng/cnpeng_demos2017_01/temp.java
+++ app/src/main/java/com/cnpeng/cnpeng_demos2017_01/temp.java
@@ -1,6 +1,15 @@
 package com.cnpeng.cnpeng_demos2017_01;
 
+import android.app.DatePickerDialog;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.ContextThemeWrapper;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.LinearLayout;
+import android.widget.NumberPicker;
+
+import java.util.Calendar;
 
 /**
  * 作者：CnPeng
@@ -11,6 +20,50 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class temp extends AppCompatActivity {
-    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+        final Calendar calendar = Calendar.getInstance();
+        int yy = calendar.get(Calendar.YEAR);
+        int mm = calendar.get(Calendar.MONTH);
+        int dd = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog dlg = new DatePickerDialog(new ContextThemeWrapper(this, android.R.style
+                .Theme_Holo_Light_Dialog_NoActionBar), null, yy, mm, dd) {
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                LinearLayout mSpinners = (LinearLayout) findViewById(getContext().getResources().getIdentifier
+                        ("android:id/pickers", null, null));
+                if (mSpinners != null) {
+                    NumberPicker mMonthSpinner = (NumberPicker) findViewById(getContext().getResources()
+                            .getIdentifier("android:id/month", null, null));
+                    NumberPicker mYearSpinner = (NumberPicker) findViewById(getContext().getResources().getIdentifier
+                            ("android:id/year", null, null));
+                    mSpinners.removeAllViews();
+                    if (mMonthSpinner != null) {
+                        mSpinners.addView(mMonthSpinner);
+                    }
+                    if (mYearSpinner != null) {
+                        mSpinners.addView(mYearSpinner);
+                    }
+                }
+                View dayPickerView = findViewById(getContext().getResources().getIdentifier("android:id/day", null, 
+                        null));
+                if (dayPickerView != null) {
+                    dayPickerView.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onDateChanged(DatePicker view, int year, int month, int day) {
+                super.onDateChanged(view, year, month, day);
+                setTitle("请选择信用卡有效期");
+            }
+        };
+        dlg.setTitle("请选择信用卡有效期");
+        dlg.show();
+
+    }
 }
