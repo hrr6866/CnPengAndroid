baf6f18d9e7398a06bcb50c45e670da13f5c3817
CnPeng <893612134@qq.com>
2017-12-05 12:01:08 +0800

    b_26 将图片存储在公共目录中，解决拍照后图库看不到的情况

--- app/src/main/java/com/cnpeng/cnpeng_demos2017_01/b_26_TakePhotoWithSysMethod/TakePhotoActivity.java
+++ app/src/main/java/com/cnpeng/cnpeng_demos2017_01/b_26_TakePhotoWithSysMethod/TakePhotoActivity.java
@@ -14,6 +14,7 @@ import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
@@ -31,6 +32,8 @@ import com.cnpeng.cnpeng_demos2017_01.databinding.ActivityTakephotoBinding;
 import java.io.File;
 import java.io.FileNotFoundException;
 
+import static android.os.Environment.DIRECTORY_PICTURES;
+
 /**
  * 作者：CnPeng
  * <p>
@@ -44,16 +47,22 @@ import java.io.FileNotFoundException;
  * 4、开启APP对应的设置界面
  * 5、使用 FileProvider 解决7.0及以后系统中使用 Uri.fromUri() 获取URI之后调用相机崩溃的情况
  * 6、getExternalCacheDir()获取当前APP对应的缓存目录，使用该方式不用申请读写SD的权限
+ * <p>
+ * 注意：
+ * 不同品牌的手机对拍照处理不一样，个别手机中即便我们没有指定存储路径，也会存储照片到默认的地址中。如：Galaxy Note4 调用系统相机执行拍照时，
+ * 如果么有指定照片存储路径，则会存储在 DCIM/Camera 目录下，此时，即便不调用 刷新相册的方法，也会执行刷新操作。
  */
 public class TakePhotoActivity extends AppCompatActivity {
 
     private ActivityTakephotoBinding binding;
     private File                     file;
-    private       String takePhotoMode          = "";
-    private final String MODE_TAKE_AND_SAVE     = "takePhotoAndSaveToLocal";
-    private final String MODE_TAKE_AND_NOT_SAVE = "takePhotoAndNotSaveToLocal";
-    private final String PERMISSION_CAMERA      = Manifest.permission.CAMERA;
-    private final int    REQUEST_CODE           = 0;    //申请权限时的请求码
+    private       String   takePhotoMode          = "";
+    private final String   MODE_TAKE_AND_SAVE     = "takePhotoAndSaveToLocal";
+    private final String   MODE_TAKE_AND_NOT_SAVE = "takePhotoAndNotSaveToLocal";
+    private final int      REQUEST_CODE1          = 0;    //申请权限时的请求码
+    private final int      REQUEST_CODE2          = 1;    //申请权限时的请求码
+    private final int      REQUEST_CODE3          = 2;    //申请权限时的请求码
+    private final String[] DENIED_DESC            = {"没有拍照权限将不能使用拍照功能", "没有存储权限将不能存储照片到本地"};  //权限被拒绝的描述文本
 
     @Override
     protected void onCreate(
@@ -84,13 +93,26 @@ public class TakePhotoActivity extends AppCompatActivity {
     }
 
     private void takePhotoOrRequestPermission() {
-        boolean isPermissionGranted = isPermissionGranted(TakePhotoActivity.this, PERMISSION_CAMERA);
-        if (isPermissionGranted) {
+        String PERMISSION_CAMERA = Manifest.permission.CAMERA;
+        boolean isCamearPermissionGranted = isPermissionGranted(TakePhotoActivity.this, PERMISSION_CAMERA);
+        String PERMISSION_EXTERNAL_STORAGE = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+        boolean isExternalStoragePermissionGranted = isPermissionGranted(TakePhotoActivity.this, 
+                PERMISSION_EXTERNAL_STORAGE);
+
+        if (isCamearPermissionGranted && isExternalStoragePermissionGranted) {
             //如果权限已经被允许
             openSysCameraView();
+        } else if (isExternalStoragePermissionGranted && !isCamearPermissionGranted) {
+            //如果拍照权限未被允许，申请拍照权限
+            requestNecessaryPermission(TakePhotoActivity.this, new String[]{PERMISSION_CAMERA}, REQUEST_CODE1);
+        } else if (isCamearPermissionGranted && !isExternalStoragePermissionGranted) {
+            //存储权限未被允许
+            requestNecessaryPermission(TakePhotoActivity.this, new String[]{PERMISSION_EXTERNAL_STORAGE}, 
+                    REQUEST_CODE2);
         } else {
-            //如果权限未被允许，申请权限
-            requestNecessaryPermission(TakePhotoActivity.this, new String[]{PERMISSION_CAMERA}, REQUEST_CODE);
+            //啥子权限都没有
+            requestNecessaryPermission(TakePhotoActivity.this, new String[]{PERMISSION_EXTERNAL_STORAGE, 
+                    PERMISSION_CAMERA}, REQUEST_CODE3);
         }
     }
 
@@ -106,12 +128,39 @@ public class TakePhotoActivity extends AppCompatActivity {
                                            @NonNull
                                                    int[] grantResults) {
         switch (requestCode) {
-            case REQUEST_CODE:
+            case REQUEST_CODE1:
                 //如果有权限被允许了——因为此处值申请了一个拍照权限，所以只要被允许了就可以了
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     openSysCameraView();
                 } else {
-                    showDeniedDialog(TakePhotoActivity.this);
+                    showDeniedDialog(TakePhotoActivity.this, DENIED_DESC[0]);
+                }
+                break;
+            case REQUEST_CODE2:
+                //如果有权限被允许了——因为此处值申请了一个拍照权限，所以只要被允许了就可以了
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openSysCameraView();
+                } else {
+                    showDeniedDialog(TakePhotoActivity.this, DENIED_DESC[1]);
+                }
+                break;
+            case REQUEST_CODE3:
+                if (grantResults.length > 0) {
+                    boolean isAllGranted = true;
+                    StringBuilder deniedHitDesc = new StringBuilder();    //权限被拒绝是的提示文本
+                    for (int i = 0; i < grantResults.length; i++) {
+                        boolean isCurGranted = grantResults[i] == PackageManager.PERMISSION_GRANTED;
+                        if (!isCurGranted) {
+                            deniedHitDesc.append(DENIED_DESC[i]).append("\n");
+                        }
+                        isAllGranted = isAllGranted && isCurGranted;
+                    }
+
+                    if (isAllGranted) {
+                        openSysCameraView();
+                    } else {
+                        showDeniedDialog(TakePhotoActivity.this, deniedHitDesc.toString());
+                    }
                 }
                 break;
             default:
@@ -124,10 +173,10 @@ public class TakePhotoActivity extends AppCompatActivity {
     /**
      * 展示被拒绝的弹窗
      */
-    private void showDeniedDialog(final Context context) {
+    private void showDeniedDialog(final Context context, String message) {
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
         builder.setTitle("提示");
-        builder.setMessage("没有拍照权限将不能使用拍照功能");
+        builder.setMessage(message);
         builder.setNegativeButton("就不给你权限", null);
         builder.setPositiveButton("我要重新开启权限", new DialogInterface.OnClickListener() {
             @Override
@@ -175,9 +224,12 @@ public class TakePhotoActivity extends AppCompatActivity {
 
         if (MODE_TAKE_AND_SAVE.equals(takePhotoMode)) {
             //如果是拍照并存储，则需要指定在本地的存储路径，并需要获取拍照之后的结果
-            file = new File(getExternalCacheDir(), System.currentTimeMillis() + "temp.jpg");
+            //file = new File(getExternalCacheDir(), System.currentTimeMillis() + "temp.jpg");
             // file = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), System.currentTimeMillis() + "temp" 
             //         + ".jpg");
+
+            file = new File(Environment.getExternalStoragePublicDirectory(DIRECTORY_PICTURES), System
+                    .currentTimeMillis() + "temp.jpg");
             Uri photoURI;
             photoURI = getPhotoUri(file);
 
