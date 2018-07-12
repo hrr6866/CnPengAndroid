16917e4269b35791a7dfed4fcc1bf0c7817cc678
CnPeng <893612134@qq.com>
2018-07-12 15:10:07 +0800

    b_34 增加try catch分支，解决部分手机无法打开通知设置界面的情况

--- app/src/main/java/com/cnpeng/cnpeng_demos2017_01/b_34_checkNotify/CheckNotifyActivity.java
+++ app/src/main/java/com/cnpeng/cnpeng_demos2017_01/b_34_checkNotify/CheckNotifyActivity.java
@@ -2,6 +2,7 @@ package com.cnpeng.cnpeng_demos2017_01.b_34_checkNotify;
 
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.Nullable;
@@ -52,25 +53,38 @@ public class CheckNotifyActivity extends AppCompatActivity {
         mBinding.tvMsg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 根据isOpened结果，判断是否需要提醒用户跳转AppInfo页面，去打开App通知权限
-                Intent intent = new Intent();
+                try {
+                    // 根据isOpened结果，判断是否需要提醒用户跳转AppInfo页面，去打开App通知权限
+                    Intent intent = new Intent();
+                    intent.setAction(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
+                    //这种方案适用于 API 26, 即8.0（含8.0）以上可以用
+                    intent.putExtra(EXTRA_APP_PACKAGE, getPackageName());
+                    intent.putExtra(EXTRA_CHANNEL_ID, getApplicationInfo().uid);
 
+                    //这种方案适用于 API21——25，即 5.0——7.1 之间的版本可以使用
+                    intent.putExtra("app_package", getPackageName());
+                    intent.putExtra("app_uid", getApplicationInfo().uid);
 
-                intent.setAction(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
-                //这种方案适用于 API 26, 即8.0（含8.0）以上可以用
-                intent.putExtra(EXTRA_APP_PACKAGE, getPackageName());
-                intent.putExtra(EXTRA_CHANNEL_ID, getApplicationInfo().uid);
+                    // 小米6 -MIUI9.6-8.0.0系统，是个特例，通知设置界面只能控制"允许使用通知圆点"——然而这个玩意并没有卵用，我想对雷布斯说：I'm not ok!!!
+                    //  if ("MI 6".equals(Build.MODEL)) {
+                    //      intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    //      Uri uri = Uri.fromParts("package", getPackageName(), null);
+                    //      intent.setData(uri);
+                    //      // intent.setAction("com.android.settings/.SubSettings");
+                    //  }
+                    startActivity(intent);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    // 出现异常则跳转到应用设置界面：锤子坚果3——OC105 API25
+                    Intent intent = new Intent();
 
-                //这种方案适用于 API21——25，即 5.0——7.1 之间的版本可以使用
-                intent.putExtra("app_package", getPackageName());
-                intent.putExtra("app_uid", getApplicationInfo().uid);
-
-                //下面这种方案是直接跳转到当前应用的设置界面。
-                //https://blog.csdn.net/ysy950803/article/details/71910806
-                // intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                // Uri uri = Uri.fromParts("package", getPackageName(), null);
-                // intent.setData(uri);
-                startActivity(intent);
+                    //下面这种方案是直接跳转到当前应用的设置界面。
+                    //https://blog.csdn.net/ysy950803/article/details/71910806
+                    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    Uri uri = Uri.fromParts("package", getPackageName(), null);
+                    intent.setData(uri);
+                    startActivity(intent);
+                }
             }
         });
     }
