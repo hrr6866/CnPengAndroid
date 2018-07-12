80e2e36786ea6cf7efafe0267ad89088fd024221
CnPeng <893612134@qq.com>
2018-07-12 09:03:22 +0800

    b_34_修改检测时机，获取焦点就检测

--- app/src/main/java/com/cnpeng/cnpeng_demos2017_01/b_34_checkNotify/CheckNotifyActivity.java
+++ app/src/main/java/com/cnpeng/cnpeng_demos2017_01/b_34_checkNotify/CheckNotifyActivity.java
@@ -26,28 +26,30 @@ import static android.provider.Settings.EXTRA_CHANNEL_ID;
  * https://juejin.im/post/5a2508656fb9a0450407b638
  */
 public class CheckNotifyActivity extends AppCompatActivity {
+    ActivityCheckNotifyBinding mBinding;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityCheckNotifyBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_check_notify);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_check_notify);
+        initClickListener();
+    }
 
-        NotificationManagerCompat manager = NotificationManagerCompat.from(this);
-        // areNotificationsEnabled方法的有效性官方只最低支持到API 19，低于19的仍可调用此方法不过只会返回true，即默认为用户已经开启了通知。
-        boolean isOpened = manager.areNotificationsEnabled();
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkNotifySetting();
+    }
 
-        if (isOpened) {
-            binding.tvMsg.setText("通知权限已经被打开" +
-                    "\n手机型号:" + android.os.Build.MODEL +
-                    "\nSDK版本:" + android.os.Build.VERSION.SDK +
-                    "\n系统版本:" + android.os.Build.VERSION.RELEASE +
-                    "\n软件包名:" + getPackageName());
-
-        } else {
-            binding.tvMsg.setText("还没有开启通知权限，点击去开启");
-        }
-
+    /**
+     * 作者：CnPeng
+     * 时间：2018/7/12 上午8:02
+     * 功用：初始化点击事件
+     * 说明：
+     */
+    private void initClickListener() {
         //CnPeng 2018/7/12 上午7:08 跳转到通知设置界面
-        binding.tvMsg.setOnClickListener(new View.OnClickListener() {
+        mBinding.tvMsg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // 根据isOpened结果，判断是否需要提醒用户跳转AppInfo页面，去打开App通知权限
@@ -71,5 +73,28 @@ public class CheckNotifyActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    /**
+     * 作者：CnPeng
+     * 时间：2018/7/12 上午9:02
+     * 功用：检查是否已经开启了通知权限
+     * 说明：
+     */
+    private void checkNotifySetting() {
+        NotificationManagerCompat manager = NotificationManagerCompat.from(this);
+        // areNotificationsEnabled方法的有效性官方只最低支持到API 19，低于19的仍可调用此方法不过只会返回true，即默认为用户已经开启了通知。
+        boolean isOpened = manager.areNotificationsEnabled();
+
+        if (isOpened) {
+            mBinding.tvMsg.setText("通知权限已经被打开" +
+                    "\n手机型号:" + android.os.Build.MODEL +
+                    "\nSDK版本:" + android.os.Build.VERSION.SDK +
+                    "\n系统版本:" + android.os.Build.VERSION.RELEASE +
+                    "\n软件包名:" + getPackageName());
+
+        } else {
+            mBinding.tvMsg.setText("还没有开启通知权限，点击去开启");
+        }
     }
 }
