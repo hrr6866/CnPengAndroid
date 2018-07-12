4c63bcf5a9234ba3707d71df936c54bd0d1ad2f6
CnPeng <893612134@qq.com>
2018-07-12 08:59:07 +0800

    b_34_修改点击事件

--- app/src/main/java/com/cnpeng/cnpeng_demos2017_01/b_34_checkNotify/CheckNotifyActivity.java
+++ app/src/main/java/com/cnpeng/cnpeng_demos2017_01/b_34_checkNotify/CheckNotifyActivity.java
@@ -44,30 +44,32 @@ public class CheckNotifyActivity extends AppCompatActivity {
 
         } else {
             binding.tvMsg.setText("还没有开启通知权限，点击去开启");
-            binding.tvMsg.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // 根据isOpened结果，判断是否需要提醒用户跳转AppInfo页面，去打开App通知权限
-                    Intent intent = new Intent();
-
-
-                    intent.setAction(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
-                    //这种方案适用于 API 26, 即8.0（含8.0）以上可以用
-                    intent.putExtra(EXTRA_APP_PACKAGE, getPackageName());
-                    intent.putExtra(EXTRA_CHANNEL_ID, getApplicationInfo().uid);
-
-                    //这种方案适用于 API21——25，即 5.0——7.1 之间的版本可以使用
-                    intent.putExtra("app_package", getPackageName());
-                    intent.putExtra("app_uid", getApplicationInfo().uid);
-
-                    //下面这种方案是直接跳转到当前应用的设置界面。
-                    //https://blog.csdn.net/ysy950803/article/details/71910806
-                    // intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                    // Uri uri = Uri.fromParts("package", getPackageName(), null);
-                    // intent.setData(uri);
-                    startActivity(intent);
-                }
-            });
         }
+
+        //CnPeng 2018/7/12 上午7:08 跳转到通知设置界面
+        binding.tvMsg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 根据isOpened结果，判断是否需要提醒用户跳转AppInfo页面，去打开App通知权限
+                Intent intent = new Intent();
+
+
+                intent.setAction(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
+                //这种方案适用于 API 26, 即8.0（含8.0）以上可以用
+                intent.putExtra(EXTRA_APP_PACKAGE, getPackageName());
+                intent.putExtra(EXTRA_CHANNEL_ID, getApplicationInfo().uid);
+
+                //这种方案适用于 API21——25，即 5.0——7.1 之间的版本可以使用
+                intent.putExtra("app_package", getPackageName());
+                intent.putExtra("app_uid", getApplicationInfo().uid);
+
+                //下面这种方案是直接跳转到当前应用的设置界面。
+                //https://blog.csdn.net/ysy950803/article/details/71910806
+                // intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                // Uri uri = Uri.fromParts("package", getPackageName(), null);
+                // intent.setData(uri);
+                startActivity(intent);
+            }
+        });
     }
 }
